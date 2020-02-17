9e34b96ccbbc4c74b2c96a13669fc13d7ecd2482
Roma <r.chikhladze@adjarabet.com>
2020-02-17 18:27:10 +0400

    some events

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,25 @@
-io();
+const socket = io();
+
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+
+document.getElementById('messageForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const message = event.target.elements.message.value;
+    socket.emit('sendMessage', message);
+});
+
+
+document.getElementById('sendLocation').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('you cant send geolocation , cause it is not supported');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit('sendLocation', {lat: latitude, long: longitude});
+    });
+});
--- src/app.js
+++ src/app.js
@@ -11,12 +11,28 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
  
+io.on('connection', (socket) => {
+    console.log('Client Connected with socket id: ',socket.id);
 
-io.on('connection', () => {
-    console.log('olaaa');
+    socket.on('sendMessage', (message) => {
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'user has left');
+    });
+
+    socket.on('sendLocation', (coords) => {
+        socket.broadcast.emit('message', {
+            user: socket.id,
+            address: `https://google.com/maps?q=${coords.lat},${coords.long}`
+        });
+    })
+
+    
 })
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`app listening to ${port} port`);
 });
